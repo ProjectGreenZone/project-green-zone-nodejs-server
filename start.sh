1f45b4502fb78081d5928e0be95dde9703ee2987
Pasindu Perera <pasindubawantha@gmail.com>
2019-11-08 01:09:44 +0530

    added https

--- start.sh
+++ start.sh
@@ -1,3 +1,5 @@
 sudo cp /etc/letsencrypt/live/dexternet.southeastasia.cloudapp.azure.com/privkey.pem /home/ubuntu/privkey.pem
+sudo chmod 777 /home/ubuntu/privkey.pem
 sudo cp /etc/letsencrypt/live/dexternet.southeastasia.cloudapp.azure.com/fullchain.pem /home/ubuntu/fullchain.pem
+sudo chmod 777 /home/ubuntu/fullchain.pem
 nodemon server-production.js
