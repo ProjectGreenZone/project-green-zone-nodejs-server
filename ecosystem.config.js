24729012123eb66075099e5d0c5b82c7e786c7ec
Pasindu Perera <pasindubawantha@gmail.com>
2019-11-07 22:27:56 +0530

    Added webhook

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,15 +1,20 @@
-module.exports = {
-    apps : [{
-      name: "server",
-      script: "./server.js",
-      error_file: '/home/ubuntu/www/err.log',
-        out_file: '/home/ubuntu/www/out.log',
-        log_file: '/home/ubuntu/www/combined.log',
-    //   env: {
-    //     NODE_ENV: "development",
-    //   },
-    //   env_production: {
-    //     NODE_ENV: "production",
-    //   }
-    }]
-  }
+// module.exports = {
+//     apps : [{
+      
+//     //   env: {
+//     //     NODE_ENV: "development",
+//     //   },
+//     //   env_production: {
+//     //     NODE_ENV: "production",
+//     //   }
+//     }]
+//   }
+
+  modules.exports = [{
+    name: "server",
+    script: "./server.js",
+    error_file: '/home/ubuntu/www/err.log',
+    out_file: '/home/ubuntu/www/out.log',
+    log_file: '/home/ubuntu/www/combined.log',
+    time: true
+  }]
