e8d2670677cc75905c1bddaf3e32e18889885e93
Pasindu Perera <pasindubawantha@gmail.com>
2019-11-07 20:49:07 +0530

    Added webhook

---
+++ webhook/hook.js
@@ -0,0 +1,20 @@
+// Listen on port 9001
+var gith = require('gith').create( 9001 );
+// Import execFile, to run our bash script
+var execFile = require('child_process').exec;
+
+gith({
+    repo: 'ProjectGreenZone/project-green-zone-nodejs-server'
+}).on( 'all', function( payload ) {
+    if( payload.branch === 'master' )
+    {
+            // Exec a shell script
+            execFile('hook.sh', function(error, stdout, stderr) {
+                console.log(stdout);
+                console.log(stderr);
+                if (error !== null) {
+                    console.log(`exec error: ${error}`);
+                }
+            });
+    }
+});
---
+++ webhook/hook.sh
@@ -0,0 +1,3 @@
+echo "Running Hook Script !"
+
+nodemon ../server.js
