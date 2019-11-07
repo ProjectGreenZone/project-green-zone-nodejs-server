89a97e4f273908e169fb02a436e94313534aa565
Pasindu Perera <pasindubawantha@gmail.com>
2019-11-08 01:07:45 +0530

    added https

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,6 +1,6 @@
 module.exports = [{
     name: "gz-server",
-    script: "./server-production.js",
+    script: "./start.sh",
     error_file: '/home/ubuntu/www/logs/gz-server-err.log',
     out_file: '/home/ubuntu/www/logs/gz-server-out.log',
     log_file: '/home/ubuntu/www/logs/gz-server-combined.log',
