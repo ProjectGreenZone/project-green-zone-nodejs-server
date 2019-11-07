9f95cfd0c2d76458ee69437ac783308aade697d4
Pasindu Perera <pasindubawantha@gmail.com>
2019-11-07 22:51:16 +0530

    Added webhook

--- webhook/hook.sh
+++ webhook/hook.sh
@@ -2,9 +2,9 @@ cd ..
 echo +++++ PWD : `pwd` +++++ 
 echo +++++ Running git pull +++++ 
 git pull
-echo +++++ Running pm2 restart gz-server +++++
-pm2 restart gz-server
 echo +++++ Removing log files ! +++++
 rm '/home/ubuntu/www/logs/gz-server-err.log'
 rm '/home/ubuntu/www/logs/gz-server-out.log'
 rm '/home/ubuntu/www/logs/gz-server-combined.log'
+echo +++++ Running pm2 restart gz-server +++++
+pm2 restart gz-server
