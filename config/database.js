f07497cf1bb70833737f30496a0aa547c399f9b9
Pasindu Perera <pasindubawantha@gmail.com>
2019-11-09 01:47:32 +0530

    deploying

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 module.exports = {
   //database: 'mongodb://<username>:<password>@ds12226.mlab.com:12226/meanauthapp',   //prod
-  database: 'mongodb+srv://admin:pass123@cluster0-dvpyf.azure.mongodb.net/test?retryWrites=true&w=majority',    //dev
+  prod_database: 'mongodb+srv://admin:pass123@cluster0-dvpyf.azure.mongodb.net/test?retryWrites=true&w=majority',    //dev
   secret: 'yoursecret',
-  prod_database: 'mongodb://localhost:27017/givemyspace-db',    //dev
+  database: 'mongodb://localhost:27017/givemyspace-db',    //dev
 }
