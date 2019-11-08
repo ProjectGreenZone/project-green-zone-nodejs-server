d46eff89224e532a2eff40edb4d526f280f0df02
Pasindu Perera <pasindubawantha@gmail.com>
2019-11-09 01:37:38 +0530

    deploying

--- start.sh
+++ start.sh
@@ -2,4 +2,4 @@ sudo cp /etc/letsencrypt/live/dexternet.southeastasia.cloudapp.azure.com/privkey
 sudo chmod 777 /home/ubuntu/privkey.pem
 sudo cp /etc/letsencrypt/live/dexternet.southeastasia.cloudapp.azure.com/fullchain.pem /home/ubuntu/fullchain.pem
 sudo chmod 777 /home/ubuntu/fullchain.pem
-nodemon server-production.js
+env TZ='Asia/Colombo' nodemon server-production.js
